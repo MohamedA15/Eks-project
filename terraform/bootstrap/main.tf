776a8f58efc3289ce4b18875959ea1ce6cd35146
MohamedA15 <mo684073@gmail.com>
2026-01-07 12:38:16 +0000

    Adding s3 state native locking

---
+++ terraform/bootstrap/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 1.9.0"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = var.state_bucket_name
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
