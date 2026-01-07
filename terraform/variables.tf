1e46cb55b093aedf0ded711982bad0a3b5dbde52
MohamedA15 <mo684073@gmail.com>
2026-01-07 14:21:23 +0000

    adding vpc variables

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  type    = string
+  default = "eu-west-2"
+}
+
+variable "vpc_name" {
+  type    = string
+  default = "eks-labs"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  type    = list(string)
+  default = ["eu-west-2a", "eu-west-2b"]
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.101.0/24", "10.0.102.0/24"]
+}
