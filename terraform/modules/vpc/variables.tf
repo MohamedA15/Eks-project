76dddbc5293186bc25804a2431419794a7616cbb
MohamedA15 <mo684073@gmail.com>
2026-01-07 14:17:46 +0000

    adding vpc variables

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,25 @@
+variable "vpc_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "single_nat_gateway" {
+  description = "Use one NAT Gateway instead of one per AZ"
+  type        = bool
+  default     = false
+}
