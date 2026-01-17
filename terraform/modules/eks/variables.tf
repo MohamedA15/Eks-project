7d34dcccd66cc96abf0bc5750c82d440950a7615
MohamedA15 <mo684073@gmail.com>
2026-01-17 08:52:19 +0000

    adding eks variables

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -1,34 +1,71 @@
+############################
+# EKS Cluster
+############################
+
 variable "cluster_name" {
-  description = "The name of the EKS cluster"
+  description = "Name of the EKS cluster"
   type        = string
 }
 
 variable "cluster_version" {
-  description = "The Kubernetes version for the EKS cluster"
+  description = "Kubernetes version for the EKS cluster"
   type        = string
 }
 
-variable "security_group_ids" {
-  description = "Security groups for EKS control plane"
-  type        = list(string)
+############################
+# Networking
+############################
+
+variable "vpc_id" {
+  description = "VPC ID where EKS and endpoints are deployed"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block of the VPC (used for security groups)"
+  type        = string
 }
 
 variable "subnet_ids" {
-  description = "Subnets for EKS cluster"
+  description = "Private subnet IDs for the EKS cluster and VPC interface endpoints"
   type        = list(string)
 }
 
+variable "private_route_table_ids" {
+  description = "Route table IDs for private subnets (used by S3 gateway endpoint)"
+  type        = list(string)
+}
+
+############################
+# EKS API Access
+############################
+
 variable "endpoint_private_access" {
-  type    = bool
-  default = true
+  description = "Enable private access to the EKS API server"
+  type        = bool
+  default     = true
 }
 
 variable "endpoint_public_access" {
-  type    = bool
-  default = false
+  description = "Enable public access to the EKS API server"
+  type        = bool
+  default     = false
 }
 
-variable "public_access_cidrs" {
-  type    = list(string)
-  default = []
+############################
+# Security Groups
+############################
+
+variable "security_group_ids" {
+  description = "Security group IDs attached to the EKS control plane"
+  type        = list(string)
+}
+
+############################
+# AWS Region
+############################
+
+variable "region" {
+  description = "AWS region (used for VPC endpoint service names)"
+  type        = string
 }
