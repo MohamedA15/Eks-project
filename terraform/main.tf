4ab8410cac591c43e72c254be4decaf12438b5c9
MohamedA15 <mo684073@gmail.com>
2026-01-17 08:50:58 +0000

    Adding eks infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,25 @@ module "vpc" {
   public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
 }
+
+
+module "eks" {
+  source = "./modules/eks"
+
+  # Cluster
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  # Networking 
+  vpc_id                  = module.vpc.vpc_id
+  vpc_cidr                = module.vpc.vpc_cidr
+  subnet_ids              = module.vpc.private_subnet_ids
+  private_route_table_ids = module.vpc.private_route_table_ids
+
+  # EKS API access
+  endpoint_private_access = true
+  endpoint_public_access  = false
+
+  # Region
+  region = var.region
+}
