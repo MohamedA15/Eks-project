56aa458fd4be9b653e7809cd4e89e2496a3946b6
MohamedA15 <mo684073@gmail.com>
2026-01-17 08:47:35 +0000

    Add private EKS cluster configuration

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -8,33 +8,15 @@ resource "aws_eks_cluster" "this" {
     security_group_ids      = var.security_group_ids
     endpoint_private_access = var.endpoint_private_access
     endpoint_public_access  = var.endpoint_public_access
-    public_access_cidrs     = var.public_access_cidrs
   }
-}
 
-
-
-resource "aws_iam_role" "eks_cluster" {
-  name = "${var.cluster_name}-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "cluster_policy" {
-  role       = aws_iam_role.eks_cluster.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "vpc_controller" {
-  role       = aws_iam_role.eks_cluster.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  depends_on = [
+    aws_vpc_endpoint.eks,
+    aws_vpc_endpoint.ec2,
+    aws_vpc_endpoint.sts,
+    aws_vpc_endpoint.ecr_api,
+    aws_vpc_endpoint.ecr_dkr,
+    aws_vpc_endpoint.logs,
+    aws_vpc_endpoint.s3
+  ]
 }
