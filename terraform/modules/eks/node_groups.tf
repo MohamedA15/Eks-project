60e7349dc4fcb06df04f9e18558deb634d96ab4f
MohamedA15 <mo684073@gmail.com>
2026-01-18 08:05:02 +0000

    Remove duplicate IAM policy definitions

--- terraform/modules/eks/node_groups.tf
+++ terraform/modules/eks/node_groups.tf
@@ -1,50 +1,12 @@
-############################################
-# IAM ROLE FOR EKS WORKER NODES
-############################################
 
-resource "aws_iam_role" "eks_node_role" {
-  name = "${var.cluster_name}-node-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-############################################
-# REQUIRED IAM POLICIES FOR WORKER NODES
-############################################
-
-resource "aws_iam_role_policy_attachment" "worker_node_policy" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "cni_policy" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
-
-resource "aws_iam_role_policy_attachment" "ecr_read_only" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-############################################
 # EKS MANAGED NODE GROUP
-############################################
+
 resource "aws_eks_node_group" "default" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "${var.cluster_name}-default"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
 
-  subnet_ids = var.subnet_ids
+  node_role_arn = aws_iam_role.eks_node_role.arn
+  subnet_ids    = var.subnet_ids
 
   capacity_type  = var.node_capacity_type
   instance_types = var.node_instance_types
