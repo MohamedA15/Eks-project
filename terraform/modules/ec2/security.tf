c0bb47e347ce3aab373e942baa12afa51cb64cd9
MohamedA15 <mo684073@gmail.com>
2026-01-23 11:54:07 +0000

    sg roles for ec2

---
+++ terraform/modules/ec2/security.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "bastion" {
+  name        = "${var.name}-bastion-sg"
+  description = "SSH access to bastion"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
