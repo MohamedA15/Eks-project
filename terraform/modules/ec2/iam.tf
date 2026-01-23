7bcd59de55e7c2b9525d0b333f50752196631613
MohamedA15 <mo684073@gmail.com>
2026-01-23 11:48:16 +0000

    iam role for ec2

---
+++ terraform/modules/ec2/iam.tf
@@ -0,0 +1,24 @@
+resource "aws_iam_role" "bastion" {
+  name = "${var.name}-bastion-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster" {
+  role       = aws_iam_role.bastion.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "${var.name}-bastion-profile"
+  role = aws_iam_role.bastion.name
+}
