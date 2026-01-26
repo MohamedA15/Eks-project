684a08550ec81946da5ad6b0bc8ddc757eff2386
MohamedA15 <mo684073@gmail.com>
2026-01-26 10:32:46 +0000

    adding state file roles for bastion

--- terraform/modules/ec2/iam.tf
+++ terraform/modules/ec2/iam.tf
@@ -42,3 +42,37 @@ resource "aws_iam_instance_profile" "bastion" {
   name = "${var.name}-bastion-profile"
   role = aws_iam_role.bastion.name
 }
+
+
+resource "aws_iam_role_policy" "terraform_state_s3" {
+  name = "${var.name}-terraform-state-s3"
+  role = aws_iam_role.bastion.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      # Allow listing the bucket
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket"
+        ]
+        Resource = "arn:aws:s3:::eks-terraform-state-mo"
+      },
+
+      # Allow read/write state + lock file
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::eks-terraform-state-mo/eks/terraform.tfstate",
+          "arn:aws:s3:::eks-terraform-state-mo/eks/terraform.tfstate.tflock"
+        ]
+      }
+    ]
+  })
+}
