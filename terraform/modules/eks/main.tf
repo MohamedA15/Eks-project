39264ccfe9c4c4f81d9ebcf9cd21230889c9993d
MohamedA15 <mo684073@gmail.com>
2026-01-26 17:40:23 +0000

    adding eks roles

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -43,3 +43,22 @@ resource "aws_eks_access_policy_association" "bastion_admin" {
 
   depends_on = [aws_eks_access_entry.bastion]
 }
+
+
+resource "aws_eks_access_entry" "terraform_admin" {
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = var.terraform_admin_role_arn
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "terraform_admin_admin" {
+  cluster_name  = aws_eks_cluster.this.name
+  principal_arn = var.terraform_admin_role_arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+
+  depends_on = [aws_eks_access_entry.terraform_admin]
+}
